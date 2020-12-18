2ccc1543a332b6858a9b7efa1d9a8f7df801bff0
nnk-ymmt <garden.n1234567890@gmail.com>
2020-12-18 15:59:27 +0900

    Array+にコメント追加

--- RxSwiftSample/Utility/Array+.swift
+++ RxSwiftSample/Utility/Array+.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+// 範囲外にアクセスしても落ちないでnilを返すためのextension
 extension Array {
     subscript (safe index: Int) -> Element? {
         return index >= 0 && index < self.count ? self[index] : nil
